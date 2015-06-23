382e386286781131137b25b36e86eca781a6851d
Thom7 <thomasnikkels@gmail.com>
2015-06-23 19:26:41 +0200

    Update roll.py

--- Roll/roll.py
+++ Roll/roll.py
@@ -5,7 +5,7 @@ from telex.callbacks.msgreceived import command, expand
 class RollPlugin(plugin.TelexPlugin):       
     @command('roll')
     @expand("^(\S+ ?(?P<dice>\d+)?d?(?P<sides>\d+)?)")
-    def test_callback(self, dice, sides, *, bot, msg):
+    def rollstuff(self, dice, sides, *, bot, msg):
         if dice:
             dice = 10 if int(dice) > 10 else int(dice)
             sides = 100 if int(sides) > 100 else int(sides)
