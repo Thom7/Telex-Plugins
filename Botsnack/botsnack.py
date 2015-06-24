300d36477249703e0d49ec734d3caa7c06e4248f
Thom7 <thomasnikkels@gmail.com>
2015-06-24 08:57:29 +0200

    Update botsnack.py

--- Botsnack/botsnack.py
+++ Botsnack/botsnack.py
@@ -4,12 +4,12 @@ from telex.callbacks.msgreceived import command, expand
         
 class RollPlugin(plugin.TelexPlugin):
     def __init__(self):
-        self.well_fed = False
+        self.well_fed = True
 
     @command('botsnack')
     @expand("^(\S+)")
     def feeding(self, *, bot, msg):
         response = ["Yum","Tasty"]
-        if !self.well_fed:
+        if self.well_fed:
            self.respond_to_msg(msg,(random.choice(response)))
-           self.well_fed = True
+           self.well_fed = False
