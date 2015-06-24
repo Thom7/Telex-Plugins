158e2c1c89edb2875cc4d71127d2785c979e81fd
Thom7 <thomasnikkels@gmail.com>
2015-06-24 12:16:54 +0200

    Update myinfo.py

--- Myinfo/myinfo.py
+++ Myinfo/myinfo.py
@@ -3,7 +3,13 @@ from telex.callbacks.msgreceived import command, expand
         
 class MyinfoPlugin(plugin.TelexPlugin):       
     @command('myinfo')
-    @expand("^(\S+)")
-    def test_callback(self, *, bot, msg):
-        response = "blah"
-        self.respond_to_msg(msg,response)
+    @expand("^(\S+) (?P<cmd>\S+) (?P<set>\S+)")
+    def Myinfo(self, cmd, set, *, bot, msg):
+        cmd_list = ["Location", "Name"]
+        if cmd in cmd_list:
+            Myinfocb(cmd, set)
+        response = ("Set "+cmd+" to "+set)
+        self.respond_to_msg(msg,str(response))
+    def Myinfocb(self, cmd, set)
+        cmd(set)
+        Return True
